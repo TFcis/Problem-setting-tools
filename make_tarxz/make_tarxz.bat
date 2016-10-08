22900d2ffa242ae9e9b1bfe146ab3fc4e280aa36
xiplus <huangxuanyuxiplus@gmail.com>
2016-10-08 13:04:52 +0800

    make_tarxz merge 32 & 64 bit program

--- make_tarxz/make_tarxz.bat
+++ make_tarxz/make_tarxz.bat
@@ -1,10 +1,16 @@
 @echo off
 cd /D %1%
-rem cd prob
 del ..\prob.tar /Q
-del ..\prob.tar.xz /Q 
-"C:\Program Files\7-Zip\7z.exe" a -ttar ..\prob.tar conf.json http res
-"C:\Program Files\7-Zip\7z.exe" a -txz  ..\prob.tar.xz ..\prob.tar
+del ..\prob.tar.xz /Q
+if exist "C:\Program Files\7-Zip\7z.exe" (
+	"C:\Program Files\7-Zip\7z.exe" a -ttar ..\prob.tar conf.json http res
+	"C:\Program Files\7-Zip\7z.exe" a -txz  ..\prob.tar.xz ..\prob.tar
+) else if exist "C:\Program Files (x86)\7-Zip\7z.exe" (
+	"C:\Program Files (x86)\7-Zip\7z.exe" a -ttar ..\prob.tar conf.json http res
+	"C:\Program Files (x86)\7-Zip\7z.exe" a -txz  ..\prob.tar.xz ..\prob.tar
+) else (
+	echo "7-Zip not found. Cannot make file."
+)
 del ..\prob.tar /Q
 cd ..
 pause
