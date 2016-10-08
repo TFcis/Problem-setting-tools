f40565e3e0fa78c694f24af3e7a0d7e4674f9f8d
xiplus <huangxuanyuxiplus@gmail.com>
2016-10-08 11:09:43 +0800

    TOJ-problem-generator update

--- TOJ-problem-generator/toj-problem.php
+++ TOJ-problem-generator/toj-problem.php
@@ -9,80 +9,75 @@ function getfoldername($text){
 	if($text[strlen($text)-1]=="\\")$text=substr($text,0,-1);
 	return $text;
 }
-fwrite(STDOUT, "Main output folder: ");
-$folder=getstdinline();
-$folder=getfoldername($folder);
-system("md \"".$folder."\"");
-while(true){
-	fwrite(STDOUT, "Sub output folder (question name): ");
-	$name=getstdinline();
-	$subfolder=$folder."\\".$name;
-	system("md \"".$subfolder."\"");
-	fwrite(STDOUT, "-----".$name."-----\n");
-	system("md \"".$subfolder."\\http\"");
-	fwrite(STDOUT, "question file: ");
-	$input=getstdinline();
-	echo "copy \"".$input."\" \"".$subfolder."\\http\\\"";
-	system("copy \"".$input."\" \"".$subfolder."\\http\\cont.".pathinfo($input, PATHINFO_EXTENSION)."\"");
-	fwrite(STDOUT, "timelimit: ");
-	$input=getstdinline();
-	$timelimit=(int)$input;
-	fwrite(STDOUT, "memlimit: ");
-	$input=getstdinline();
-	$memlimit=(int)$input;
-	fwrite(STDOUT, "testdata folder: ");
-	$testdatafolder=getstdinline();
-	$testdatafolder=getfoldername($testdatafolder);
-	$testdatalist=array();
-	system("md \"".$subfolder."\\res\\testdata\"")."\n";
-	if($handle=opendir($testdatafolder)){
-		while (($file = readdir($handle))!==false) {
-			$filename=pathinfo($file);
-			if(is_numeric($filename["filename"])){
-				$testdatalist[$filename["filename"]][$filename["extension"]]=true;
-			}
-		}
-		closedir($handle);
-	}else {
-		fwrite(STDOUT, "Error: opendir\n");
+echo "Output folder >";
+$folder = trim(fgets(STDIN));
+mkdir($folder);
+chdir($folder);
+mkdir("http");
+mkdir("res");
+mkdir("res/testdata");
+echo "Problem file >";
+$input = trim(fgets(STDIN));
+copy($input, "http/cont.".pathinfo($input, PATHINFO_EXTENSION));
+echo "timelimit >";
+$timelimit = (int)trim(fgets(STDIN));
+echo "memlimit >";
+$memlimit = (int)trim(fgets(STDIN));
+echo "testdata folder >";
+$testdatafolder = trim(fgets(STDIN));
+$filecheck = array();
+$filelist = scandir($testdatafolder);
+foreach ($filelist as $file) {
+	$filename = pathinfo($file);
+	if (is_numeric($filename["filename"])) {
+		$filecheck[$filename["filename"]][$filename["extension"]] = true;
 	}
-	foreach ($testdatalist as $key => $value) {
-		if($value["in"]&&$value["out"]){
-			system("copy \"".$testdatafolder."\\".$key.".in\" \"".$subfolder."\\res\\testdata\\".$key.".in\"");
-			system("copy \"".$testdatafolder."\\".$key.".out\" \"".$subfolder."\\res\\testdata\\".$key.".out\"");
-		}else if($value["in"]){
-			echo $key." no out\n";
-		}else if($value["out"]){
-			echo $key." no in\n";
-		}
-	}
-	$conf=new stdClass;
-	$conf->timelimit=$timelimit;
-	$conf->memlimit=$memlimit;
-	$conf->compile="g++";
-	$conf->score="rate";
-	$conf->check="diff";
-	$conf->test=array();
-	while(count($testdatalist)!=0){
-		$input=0;
-		$templist=array();
-		while($input!=-1){
-			echo "input testdata index (end with -1): ";
-			$index=getstdinline();
-			if($index==-1)break;
-			else if(preg_match("/(\d+)-(\d+)/",$index,$match)){
-				for($i=$match[1];$i<=$match[2];$i++){
-					$templist[]=(int)$i;
-				}
-			}else {
-				$templist[]=(int)$index;
-			}
-		}
-		echo "input testdata ".implode(",",$templist)." score (end with -1): ";
-		$score=getstdinline();
-		if($score==-1)break;
-		$conf->test[]=array("data"=>$templist,"weight"=>(int)$score);
-	}
-	file_put_contents($subfolder."\\conf.json", json_encode($conf, JSON_PRETTY_PRINT));
 }
+$indexlist = array();
+foreach ($filecheck as $key => $value) {
+	if (isset($value["in"]) && isset($value["out"])) {
+		copy($testdatafolder."/".$key.".in", "res/testdata/".(int)$key.".in");
+		copy($testdatafolder."/".$key.".out", "res/testdata/".(int)$key.".out");
+		echo "find ".$key.".in and ".$key.".out . copid.\n";
+		$indexlist[(int)$key] = (int)$key;
+	} else if(isset($value["in"])) {
+		echo "find ".$key.".in but ".$key.".out is missing. ignored\n";
+	} else if(isset($value["out"])) {
+		echo "find ".$key.".out but ".$key.".in is missing. ignored\n";
+	}
+}
+var_dump($indexlist);
+asort($indexlist);
+var_dump($indexlist);
+$conf = new stdClass;
+$conf->timelimit = $timelimit;
+$conf->memlimit = $memlimit;
+$conf->compile = "g++";
+$conf->score = "rate";
+$conf->check = "diff";
+$conf->test = array();
+while (true) {
+	$templist = array();
+	echo "input testdata index (format:1-4,5,6,7-9)(empty string to exit)\n";
+	echo "not used: ".implode(",", $indexlist)."\n";
+	echo ">";
+	$input = trim(fgets(STDIN));
+	if ($input == "") break;
+	foreach (explode(",", $input) as $temp) {
+		if (preg_match("/(\d+)-(\d+)/", $input, $match)) {
+			for ($i = (int)$match[1]; $i <= (int)$match[2]; $i++) {
+				$templist[] = $i;
+				unset($indexlist[$i]);
+			}
+		} else {
+			$templist[] = (int)$temp;
+			unset($indexlist[(int)$temp]);
+		}
+	}
+	echo "input testdata ".implode(",",$templist)." score >";
+	$score = (int)trim(fgets(STDIN));
+	$conf->test[] = array("data"=>$templist, "weight"=>$score);
+}
+file_put_contents("conf.json", json_encode($conf, JSON_PRETTY_PRINT));
+echo "Done!\n";
 ?>
